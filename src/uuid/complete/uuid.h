9e742b5a75c0eda4f379df34102355c642b43e39
emoney <eric.mcmullen@gmail.com>
2022-11-07 07:07:47 -0700

    .

--- src/uuid/complete/uuid.h
+++ src/uuid/complete/uuid.h
@@ -2,10 +2,24 @@
 #define UUID_H_
 
 #include <string>
-
+#include <limits>
 
 namespace uuid
 {
+   // TODO: Think about int_t and the like
+
+   namespace internal
+   {
+      unsigned int getLowBits(unsigned long i)
+      {
+         return i & 0xffffffff;
+      }
+
+      unsigned short getMiddleBits(unsigned long i){
+         return (i >> 24) & 0xffff ;
+      }
+
+   }
 
    std::string generateUUID()
    {
--- src/uuid/complete/uuid_tests.cc
+++ src/uuid/complete/uuid_tests.cc
@@ -1,15 +1,39 @@
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
 #include "uuid.h"
+#include <limits>
 
 using namespace uuid;
 
 namespace
 {
 
-    TEST(generateUUID, ShouldhandleTheDefaultCase)
+    TEST(getLowBits, ShouldPreserveLowBits)
     {
-        EXPECT_EQ("1", "1");
+        const unsigned long expected = 5;
+        const auto actual = uuid::internal::getLowBits(expected);
+        EXPECT_EQ(expected, actual);
+    }
+
+    TEST(getLowBits, ShouldThrowAwayHighBits)
+    {
+        const auto expected = 5;
+        const auto actual = uuid::internal::getLowBits(expected | 0xFF00000000000000);
+        EXPECT_EQ(expected, actual);
+    }
+
+    TEST(getMiddleBits, ShouldKeepCenterBits)
+    {
+        const unsigned short expected = 5;
+        const auto actual = uuid::internal::getMiddleBits(expected << 24);
+        EXPECT_EQ(expected, actual);
+    }
+
+    TEST(getMiddleBits, ShouldThrowOutWingBits)
+    {
+        const unsigned short expected = 5;
+        const auto actual = uuid::internal::getMiddleBits((expected << 24)| 0xFFFFFF0000FFFFFF);
+        EXPECT_EQ(expected, actual);
     }
 
 }
