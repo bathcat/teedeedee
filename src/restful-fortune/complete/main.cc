357a5f960a6e7943e345b85bab306edc25ed5696
emoney <eric.mcmullen@gmail.com>
2022-11-07 09:24:39 -0700

    .

---
+++ src/restful-fortune/complete/main.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "fortune.h"
+
+#include "httplib.h"
+
+const std::string usage = R"(
+Fortune
+Lorem ipsum.....
+)";
+
+
+int main(const int argc, const char**argv) {
+
+    if(argc > 1){
+        std::cout << usage << std::endl;
+        return 0;
+    }
+
+    httplib::Client cli("http://api.kanye.rest");
+    const auto res = cli.Get("/");    
+
+
+    std::cout << "Body:" << res.value().body << std::endl;
+    
+    return 0;
+}
