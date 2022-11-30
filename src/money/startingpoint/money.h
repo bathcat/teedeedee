e3ec75e42d57d34142e019409e7cbd4088985ad0
bathcat <you@example.com>
2022-11-30 20:27:57 +0000

    Add money placeholder.

---
+++ src/money/startingpoint/money.h
@@ -0,0 +1,11 @@
+#ifndef MONEY_H_
+#define MONEY_H_
+
+namespace money
+{
+   
+
+
+}
+
+#endif
---
+++ src/money/startingpoint/money_tests.cc
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "gmock/gmock.h"
+#include "money.h"
+
+using namespace money;
+
+namespace
+{
+
+    TEST(moneyNamespace, TrueShouldBeTrue)
+    {
+        EXPECT_EQ(true, true);
+    }
+
+}
